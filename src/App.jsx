976c2ddba224b3a4f77a14cdf300ab0cf339a637
Tanisha Muvel <tanimuvel1234@gmail.com>
2024-07-15 01:41:02 +0530

    text-changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
      <img className="h-full w-full" src={Logo} alt="" />
       </div>
       <div onClick={() => setisopen(!isopen)} >
-        <div  className="  flex justify-center my-3 lg:hidden  text-white text-4xl">
+        <div  className="  flex justify-center my-5 lg:hidden  text-white text-2xl">
           {
             isopen ? 
               <RxCross2 />
@@ -29,31 +29,31 @@ function App() {
           <img className="w-24 md:w-full " src={Logo} alt="" />
         </div>
       <div className=" lg:px-2  lg:py-0">
-        <ul className="list-none flex  lg:flex-row flex-col md:flex-row justify-between text-center md:pt-0 md:px-10 lg:my-0   md:my-4 text-2xl lg:text-xl lg:pt-1 xl:pt-0 xl:text-2xl md:text-2xl pt-3">
-          <li className=" font-normal py-1 md:px-3 lg:py-3 text-white  hover:text-gray-500  hover:rotate-6 " >
-            <a className="tracking-widest lg:tracking-normal" href="#">Catalog</a>
+        <ul className="list-none flex  lg:flex-row flex-col md:flex-row justify-between text-center md:pt-3 md:px-10 lg:my-0   md:my-2 text-sm md:text-xl lg:pt-1 xl:pt-0  pt-3">
+          <li className=" group font-normal  py-1 md:px-3 lg:py-3 text-white  hover:text-gray-500 " >
+            <a className="tracking-widest lg:tracking-normal" href="#">Catalog <div  class="bg-white hidden md:flex mt-2 h-1 w-0 group-hover:w-full group-hover:bg-gray-500 transition-all duration-500"></div></a>
           </li>
-          <li className=" font-normal py-1 md:px-3 lg:py-3  text-white hover:text-gray-500  hover:rotate-6">
-            <a className="tracking-widest lg:tracking-normal " href="#">Delivery</a>
+          <li className="group font-normal py-1 md:px-3 lg:py-3  text-white hover:text-gray-500 ">
+            <a className="tracking-widest lg:tracking-normal " href="#">Delivery <div class="bg-white hidden  md:flex mt-2 h-1 w-0 group-hover:w-full group-hover:bg-gray-500 transition-all duration-500"></div></a>
           </li>
-          <li className=" font-normal py-1 md:px-3 lg:py-3  text-white hover:text-gray-500 hover:rotate-6">
-            <a className="tracking-widest lg:tracking-normal" href="#">Payment</a>
+          <li className="group font-normal py-1 md:px-3 lg:py-3  text-white hover:text-gray-500 ">
+            <a className="tracking-widest lg:tracking-normal" href="#">Payment <div class="bg-white hidden md:flex mt-2 h-1 w-0 group-hover:w-full group-hover:bg-gray-500 transition-all duration-500"></div></a>
           </li>
-          <li className="font-normal py-1 md:px-3 lg:py-3  text-white hover:text-gray-500 hover:rotate-6 ">
-            <a  className="tracking-widest lg:tracking-normal"href="#">Contact</a>
+          <li className="group font-normal py-1 md:px-3 lg:py-3  text-white hover:text-gray-500  ">
+            <a  className="tracking-widest lg:tracking-normal"href="#">Contact <div class="bg-white hidden md:flex mt-2 h-1 w-0 group-hover:w-full group-hover:bg-gray-500 transition-all duration-500"></div></a>
           </li>
-          <li className=" font-normal py-1 md:px-3 lg:py-3  text-white hover:text-gray-500 hover:rotate-6">
-            <a className="tracking-widest lg:tracking-normal" href="#">About</a>
+          <li className="group font-normal py-1 md:px-3 lg:py-3  text-white hover:text-gray-500 ">
+            <a className="tracking-widest lg:-tracking-normal" href="#">About <div class="bg-white hidden md:flex mt-2 h-1 w-0 group-hover:w-full group-hover:bg-gray-500 transition-all duration-500"></div></a>
           </li>
         </ul>
         </div>
       </div>
       <div> 
         <div className="flex-col flex md:flex-row md:justify-end lg:px-1 md:px-11 pb-5 lg:pb-0 text-center lg:h-10 justify-center items-center gap-3  py-2 lg:pt-5">
-          <button className=" h-11 w-40 xl:h-11 xl:w-28 lg:h-11 lg:w-28 lg:text-2sm xl:text-xl md:h-11 md:w-36  flex items-center justify-center text-xl bg-white border-2  hover:border-white hover:text-white hover:bg-transparent  rounded">
+          <button className=" h-11 w-36 xl:h-10 xl:w-28 lg:h-10 lg:w-28 text-2sm  md:h-11 md:w-36  flex items-center justify-center  bg-white border-2  hover:border-white hover:text-white hover:bg-transparent  rounded">
           Sign In
           </button>
-          <button className=" h-11 w-40 xl:h-11 xl:w-28 lg:h-11 lg:w-28  lg:text-2sm xl:text-xl md:h-11 md:w-36 flex items-center justify-center text-xl bg-white border-2  hover:border-white hover:text-white hover:bg-transparent  rounded">
+          <button className=" h-11 w-36 xl:h-10 xl:w-28 lg:h-10 lg:w-28  text-2sm  md:h-11 md:w-36 flex items-center justify-center  bg-white border-2  hover:border-white hover:text-white hover:bg-transparent  rounded">
           Join Now
           </button>
         </div>
